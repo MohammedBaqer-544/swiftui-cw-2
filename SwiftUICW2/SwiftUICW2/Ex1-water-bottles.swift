ea51f7aafc24047b57e6a0543643dbf4a58e34f0
MohammedBaqer-544 <almousawi2002@gmail.com>
2020-12-25 20:40:38 +0300

    done

--- SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
+++ SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
@@ -5,8 +5,15 @@
 //  Created by Omar Alibrahim on 12/19/20.
 //  Copyright: Kuwait Codes 2020 code.kw
 import SwiftUI
+import AVFoundation
+
+var music: AVAudioPlayer!
 
 struct Exercise1: View {
+    @State var name = ""
+    @State var bottle = 0
+     
+    
     var body: some View {
         ZStack {
             Image("charter")
@@ -21,11 +28,53 @@ struct Exercise1: View {
                 
 //                ADD THE CODE HERE
                 
+                TextField("اكتب اسمك هنا", text: $name)
+                    .disableAutocorrection(true)
+                    
                 
-                Spacer()        
+                Stepper("كم بطلا من الماء تريد ان تتعهد بان تشرب", value: $bottle)
+                
+                Spacer()
+                
+                VStack {
+                    Text("اتعهد انا")
+                        .font(.largeTitle)
+                    
+                    Text(name)
+                        .font(.largeTitle)
+                    
+                    Text("")
+                        .font(.largeTitle)
+                    
+                    Text("ان اشرب")
+                        .font(.largeTitle)
+                    
+                    Text("\(bottle)")
+                        .font(.largeTitle)
+                    
+                    Text("امواب من الماء يوميا ")
+                        .font(.largeTitle)
+                    Text("والله على ما اقول شهيد")
+                        .font(.largeTitle)
+                    
+                }
+                
+                Spacer()
             }.padding()
+        }.onAppear {
+            playMusic()
         }
     }
+     func playMusic() {
+        if let musicURL = Bundle.main.url(forResource: "Harry Potter Song", withExtension: "mp3") {
+            if let audioPlayer = try? AVAudioPlayer(contentsOf: musicURL) {
+                music = audioPlayer
+                music.numberOfLoops = -1
+                music.play()
+            }
+        }
+    }
+
 }
 
 struct Exercise1_Previews: PreviewProvider {
@@ -33,3 +82,4 @@ struct Exercise1_Previews: PreviewProvider {
         Exercise1()
     }
 }
+
